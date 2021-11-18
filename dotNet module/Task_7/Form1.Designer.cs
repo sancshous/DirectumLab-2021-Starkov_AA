05df9a98177afed3777459458534f10d205bcd4d
sancshous <shura.starkov.2014@mail.ru>
2021-11-18 10:11:45 +0400

    Исправил замечания

--- dotNet module/Task_7/Form1.Designer.cs
+++ dotNet module/Task_7/Form1.Designer.cs
@@ -10,33 +10,7 @@ namespace Task_7
     /// </summary>
     private System.ComponentModel.IContainer components = null;
 
-    /// <summary>
-    /// Создает обьект типа RichTextBox.
-    /// </summary>
-    /// <returns>Возвращает обьект типа RichTextBox.</returns>
-    public RichTextBox CreateRichTextBox()
-    {
-      var richTextBox1 = new RichTextBox();
-      richTextBox1.Dock = DockStyle.Fill;
-      this.Controls.Add(richTextBox1);
-      return richTextBox1;
-    }
-
-    /// <summary>
-    /// Загружает данные из текстового файла, сжатого по методу GZIP, в RichTextBox.
-    /// </summary>
-    /// <param name="filename">Текстовый файл, сжатый по методу GZIP.</param>
-    /// <param name="edit">Обьект типа RichTextBox.</param>
-    public void LoadGZippedText(string filename, RichTextBox edit)
-    {
-      using (var sourceStream = new System.IO.FileStream(filename,
-              System.IO.FileMode.Open, System.IO.FileAccess.Read,
-              System.IO.FileShare.Read))
-      using (var uncompressedStream = new System.IO.Compression.GZipStream(
-              sourceStream, System.IO.Compression.CompressionMode.Decompress, true))
-      using (var textReader = new System.IO.StreamReader(uncompressedStream, true))
-        edit.Rtf = textReader.ReadToEnd();
-    }
+    
 
     /// <summary>
     ///  Clean up any resources being used.
