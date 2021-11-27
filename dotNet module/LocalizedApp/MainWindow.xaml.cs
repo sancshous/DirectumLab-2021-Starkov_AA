85c3a5696536848b1b54b631c128c6d748ce14a8
sancshous <shura.starkov.2014@mail.ru>
2021-11-27 13:32:08 +0400

    Решение практического задания 14

---
+++ dotNet module/LocalizedApp/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LocalizedApp
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    private void Button1_Click(object sender, RoutedEventArgs e)
+    {
+      var resMan = new ResourceManager("LocalizedApp.TextMessages",
+  Assembly.GetExecutingAssembly());
+      MessageBox.Show(resMan.GetString("HelloMessage",
+        new System.Globalization.CultureInfo("en")));
+
+    }
+  }
+}
