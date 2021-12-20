3a17d42db54831b1209e61c5abe7775474df9aeb
sancshous <shura.starkov.2014@mail.ru>
2021-12-20 18:13:21 +0400

    Исправил замечания

--- server/Tests/RoomControllerTests.cs
+++ server/Tests/RoomControllerTests.cs
@@ -4,12 +4,12 @@ using PlanPoker.Controllers;
 using PlanPoker.Domain.Entities;
 using PlanPoker.Domain.Repositories;
 using PlanPoker.Domain.Services;
+using PlanPoker.DTO;
 using PlanPoker.DTO.DTOBuilder;
 using PlanPoker.Infrastructure.Repositories;
 using System;
 using System.Linq;
 using System.Text.Json;
-using Tests.TestContext;
 
 namespace Tests
 {
@@ -25,17 +25,19 @@ namespace Tests
     [OneTimeSetUp]
     public void SetUpOnce()
     {
-      var userContext = new UserTestContext().Context;
-      var roomContext = new RoomTestContext().Context;
-      var discussionContext = new DiscussionTestContext().Context;
-      var cardContext = new CardTestContext().Context;
-      var voteContext = new VoteTestContext().Context;
+      var userContext = new TestContext().UserContext;
+      var roomContext = new TestContext().RoomContext;
+      var discussionContext = new TestContext().DiscusContext;
+      var cardContext = new TestContext().CardContext;
+      var voteContext = new TestContext().VoteContext;
 
       var userMock = new Mock<IRepository<User>>();
       userMock.Setup(repository => repository.Get(It.IsAny<Guid>())).Returns(new UserRepository(userContext).Get(TestData.GetTestUser().Id));
+      userMock.Setup(repository => repository.GetAll()).Returns(new UserRepository(userContext).GetAll());
       this.userService = new UserService(userMock.Object);
 
       var roomMock = new Mock<IRepository<Room>>();
+      roomMock.Setup(repository => repository.Get(It.IsAny<Guid>())).Returns(new RoomRepository(roomContext).Get(TestData.GetTestRoom().Id));
       roomMock.Setup(repository => repository.GetAll()).Returns(new RoomRepository(roomContext).GetAll());
       this.roomService = new RoomService(roomMock.Object, userMock.Object);
 
@@ -45,6 +47,7 @@ namespace Tests
 
       var discusMock = new Mock<IRepository<Discussion>>();
       discusMock.Setup(repository => repository.GetAll()).Returns(new DiscussionRepository(discussionContext).GetAll());
+      discusMock.Setup(repository => repository.Get(It.IsAny<Guid>())).Returns(new DiscussionRepository(discussionContext).Get(TestData.GetTestDiscussion().Id));
       this.discussionService = new DiscussionService(discusMock.Object, cardMock.Object);
 
       var voteMock = new Mock<IRepository<Vote>>();
@@ -57,9 +60,51 @@ namespace Tests
     public void GetRoomInfoTest()
     {
       var roomId = TestData.GetTestRoom().Id;
-      var room = this.roomService.GetRooms().First(room => room.Id == roomId);
+      var userId = TestData.GetTestUser().Id;
+      var discusId = TestData.GetTestDiscussion().Id;
+
+      this.roomService.AddUser(roomId, userId);
+      var room = this.roomService.GetRoom(roomId);
       var discussions = this.discussionService.GetDiscussions(room.Id);
-      var expected = RoomDTOBuilder.Build(room, discussions, this.cardService, this.discussionService);
+      this.discussionService.AddVote(discusId, TestData.GetTestVote());
+
+      var users =  this.userService.GetUsers().Where(user => room.Users.Contains(user)).Select(user =>
+      new UserDTO()
+      {
+        Id = user.Id,
+        Name = user.Name
+      }).ToList();
+
+      var votes = this.voteService.GetVotes(discusId).Select(vote =>
+      new VoteDTO()
+      {
+        Id = vote.Id,
+        Card = this.cardService.GetCard(vote.CardId),
+        RoomId = vote.RoomId,
+        UserId = vote.UserId,
+        DiscussionId = vote.DiscussionId
+      }).ToList();
+
+      var discussionsInRoom = discussions.Select(discussion =>
+      new DiscussionDTO()
+      {
+        Id = discussion.Id,
+        Title = discussion.Title,
+        RoomID = discussion.RoomId,
+        Start = discussion.Start,
+        End = discussion.End,
+        Votes = votes,
+        AverageVote = this.discussionService.CalculateAverageVote(discussion.Id)
+      }).ToList().Where(discussion => discussion.RoomID == room.Id);
+
+      var expected = new RoomDTO()
+      {
+        Id = room.Id,
+        Title = room.Title,
+        OwnerId = room.OwnerId,
+        Users = users,
+        Discussions = discussionsInRoom
+      };
 
       var roomController = new RoomController(this.roomService, this.discussionService, this.cardService);
       var actual = roomController.GetRoomInfo(roomId);
